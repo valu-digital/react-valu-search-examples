b9593d87f4646bd0457ef93e05352eb922a60bce
Esa-Matti Suuronen <esa-matti@suuronen.org>
2021-04-27 14:39:01 +0300

    cleanup

--- next-header-input/src/Layout.tsx
+++ next-header-input/src/Layout.tsx
@@ -9,42 +9,45 @@ export const vs = new ValuSearch({
 export function Layout(props: { children: React.ReactNode }) {
   return (
     <vs.Provider>
-      <LayoutInner {...props} />
+      <Header />
+      <Content {...props} />
     </vs.Provider>
   );
 }
 
-export function LayoutInner(props: { children: React.ReactNode }) {
+function Header() {
   const { isActive, deactivate } = vs.useStatus();
   const ref = vs.useInput();
 
   return (
-    <>
-      <header>
-        <div className="wrap">
-          <div className="title">Header Text</div>
-          <div>
-            <input
-              ref={ref}
-              className="search-input"
-              type="text"
-              placeholder="Search..."
-            />
-            <button
-              style={{
-                visibility: isActive ? "visible" : "hidden",
-              }}
-              type="button"
-              onClick={() => {
-                deactivate();
-              }}
-            >
-              x
-            </button>
-          </div>
+    <header>
+      <div className="wrap">
+        <div className="title">Header Text</div>
+        <div>
+          <input
+            ref={ref}
+            className="search-input"
+            type="text"
+            placeholder="Search..."
+          />
+          <button
+            style={{
+              visibility: isActive ? "visible" : "hidden",
+            }}
+            type="button"
+            onClick={() => {
+              deactivate();
+            }}
+          >
+            x
+          </button>
         </div>
-      </header>
-      <main>{isActive ? <vs.Results /> : props.children}</main>
-    </>
+      </div>
+    </header>
   );
+}
+
+function Content(props: { children: React.ReactNode }) {
+  const { isActive } = vs.useStatus();
+  return <main>{isActive ? <vs.Results /> : props.children}</main>;
 }
