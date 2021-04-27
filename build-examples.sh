a20b7e365b3fbd470a92a94c9db240f9746b9aba
Esa-Matti Suuronen <esa-matti@suuronen.org>
2021-04-27 12:30:20 +0300

    Fix build dir

--- build-examples.sh
+++ build-examples.sh
@@ -8,13 +8,15 @@ export NODE_ENV=production
 
 build_nextjs() {
     local name=$1
-    cd "$name"
-    npm ci
+    (
+        cd "$name"
+        npm ci
 
-    export BASE_PATH="/$name"
-    ./node_modules/.bin/next build
-    ./node_modules/.bin/next export
-    mv out "../out/$name"
+        export BASE_PATH="/$name"
+        ./node_modules/.bin/next build
+        ./node_modules/.bin/next export
+        mv out "../out/$name"
+    )
 }
 
 
