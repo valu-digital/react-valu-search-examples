6517428bce3c083deacf4d95806e075a3fc254f7
Esa-Matti Suuronen <esa-matti@suuronen.org>
2021-04-27 13:04:11 +0300

    Initial parcel demo

---
+++ parcel-fullscreen-modal/src/index.js
@@ -0,0 +1,10 @@
+import { ValuSearch } from "@valu/react-valu-search";
+
+export const vs = new ValuSearch({
+  customer: "yle",
+  apiKey: "efca53a6bbda038df8a293fd889d15d1dbf8a78c",
+});
+
+vs.initModal();
+
+vs.bindInput(document.querySelector(".search-input"));
