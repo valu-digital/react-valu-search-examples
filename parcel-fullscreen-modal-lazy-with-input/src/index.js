2469988e4a694dc8512a6a8d2d3691d953ea4d12
Joonas Varis <joonas.varis@valu.fi>
2021-06-11 17:31:06 +0300

    simplify example

--- parcel-fullscreen-modal-lazy-with-input/src/index.js
+++ parcel-fullscreen-modal-lazy-with-input/src/index.js
@@ -5,25 +5,13 @@ const loader = new LazyValuSearch({
 });
 
 loader.init(() => {
-  const button = select("button.search", HTMLButtonElement);
   const input = select(".outside-input", HTMLInputElement);
-
-  console.log(input);
-
-  button.addEventListener("click", () => {
-    if (!loader.isLoaded()) {
-      button.classList.add("loading");
-    }
-
-    loader.activate();
-  });
 
   input.addEventListener("focus", () => {
     loader.load();
   });
 
   return (vs) => {
-    button.classList.remove("loading");
     vs.bindInputAsOpener(input);
     vs.initModal();
   };
